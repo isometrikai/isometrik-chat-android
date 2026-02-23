ab1dde463a25e63c88c3f94d6c30faede25d0a1c
mayurappscripde <146091920+mayurappscripde@users.noreply.github.com>
2026-02-23 19:03:26 +0530

    - Fix: Log.e package import

--- isometrik-chat/src/main/java/io/isometrik/ui/messages/chat/messageBinders/VideoCallReceivedBinder.kt
+++ isometrik-chat/src/main/java/io/isometrik/ui/messages/chat/messageBinders/VideoCallReceivedBinder.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Typeface
 import android.text.SpannableString
 import android.text.style.StyleSpan
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
